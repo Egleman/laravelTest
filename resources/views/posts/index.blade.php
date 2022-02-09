6aa15bc7df5391ad8b15ad54673857d7f55f8ed5
Egleman <martsnov@hotmail.com>
2022-02-09 17:30:37 +0300

    Создание формы для добавления поста

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,36 +1,18 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="justify-content: space-between;">
-            <ul class="col-6 navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item active">
-                    <a class="nav-link active" aria-current="page" href="/">Главная</a>
-                </li>
-                <li class="nav-item active offset-3">
-                    <a class="nav-link active" aria-current="page" href="/">Создать пост</a>
-                </li>
-            </ul>
-            <form class="d-flex" action="{{ route('post.index') }}">
-                <input class="form-control me-2" type="search" name="search" placeholder="Найти пост..." aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Поиск</button>
-            </form>
-        </div>
-    </div>
-</nav>
+@extends('layouts.layout')
+@section('content')
 
-<div class="container">
+
+    @if(isset($_GET['search']))
+        @if(count($posts)>0)
+            <h2>Результаты поиска по запросу <?=$_GET['search']?></h2>
+            <p class="lead">Всего найдено {{ count($posts) }}</p>
+        @else
+            <h2>По запросу <?=$_GET['search']?> ничего не найдено</h2>
+            <a href="{{ route('post.index') }}" class="btn btn-outline-primary">отобразить все посты</a>
+        @endif
+    @endif
+
     <div class="row">
         @foreach($posts as $post)
         <div class="col-6">
@@ -50,6 +32,5 @@
     @if(!isset($_GET['search']))
     {{ $posts->links() }}
     @endif
-</div>
-</body>
-</html>
+    @endsection
+
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@
 
 Route::get('/', 'PostController@index');
 Route::get('post/', 'PostController@index')->name('post.index');
+Route::get('post/create', 'PostController@create')->name('post.create');
+Route::post('post/', 'PostController@store')->name('post.store');
